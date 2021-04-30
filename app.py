e77e1afa7856b5488e53292e7a1292483daf3fcc
kleverone99 <75403103+kleverone99@users.noreply.github.com>
2021-04-30 00:33:27 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine, func
 import pandas as pd
 
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+
 from config import pwd
 import json
 
@@ -23,7 +25,13 @@ airplane_events = Base.classes.airplane_events_clean
 
 
 app = Flask(__name__)
+CORS(app)
 
+# Flask Routes to render HTML
+@app.route("/")
+def home():
+    """Return the homepage."""
+    return render_template("index.html")
 
 @app.route("/api")
 def Accidents():
@@ -48,10 +56,53 @@ def Accidents():
      accident_df['longitude'] = accident_df['longitude'].astype(str)
 
      accident_dict = accident_df.to_dict()
-    # accident_dict = "I worked"
      return jsonify(accident_dict)
 
+@app.route("/monthly")
+def Monthly():
+    monthly_accidents = engine.execute ('''Select Extract (Month from event_date) AS monthid
+		                                    , TO_CHAR (event_date, 'Month') AS monthname
+		                                    , count (event_id) as accidents
+		                                    , sum (fatalities) as fatalities
+	                                        from	airplane_events_clean
+                                            group by Extract (Month from event_date), TO_CHAR (event_date, 'Month')
+                                            order by Extract (Month from event_date)''')
+    results= []
+    results = [list(row) for row in monthly_accidents]
+    test = []
+    for x in range(0, len(results)):
+        result_dict = {'monthid':results[x][0], 'monthname': results[x][1], 'accidents': results[x][2], 'fatalities': results[x][3] }
+        test.append(result_dict)
+    return jsonify(test)     
 
+
+@app.route("/weather_impact")
+def Weather():
+    fatality_flight_phase = engine.execute('''SELECT flight_phase, SUM(uninjured) as UNINJURED, SUM(FATALITIES) as FATALITIES, SUM(injuries) as INJURIES 
+                            FROM airplane_events_clean
+                            GROUP BY flight_phase
+                            ORDER BY flight_phase''')
+    results= []
+    results = [list(row) for row in fatality_flight_phase]
+    test = []
+    for x in range(0, len(results)):
+        result_dict = {'Flight Phase':results[x][0], 'Uninjured': results[x][1], 'Injuries': results[x][3], 'Fatalities': results[x][2] }
+        test.append(result_dict)
+    return jsonify(test)
+
+@app.route("/weather_flight_impact")
+def Weather_flight_phase():
+    weather_flight_phase = engine.execute('''SELECT flight_phase, airplane_events_clean.weather_condition, SUM(FATALITIES) as FATALITIES, SUM(injuries) as INJURIES FROM airplane_events_clean
+        WHERE weather_condition = 'IMC' or weather_condition = 'VMC'
+        GROUP BY flight_phase, weather_condition
+        ORDER BY flight_phase;''')
+    results= []
+    results = [list(row) for row in weather_flight_phase]
+    test = []
+    for x in range(0, len(results)):
+        result_dict = {'Flight Phase':results[x][0], 'Weather Condition': results[x][1], 'Fatal': results[x][2], 'Injuries': results[x][3] }
+        test.append(result_dict)
+    return jsonify(test)
 
 if __name__ == '__main__':
     app.run(debug=True)
